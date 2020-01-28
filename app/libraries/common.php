e3e3169702ef0256986ff62cc82589f24a7e7a76
Medini <medini.m@techjini.com>
2020-01-28 12:50:10 +0530

    APplication ID new implementation

--- app/libraries/common.php
+++ app/libraries/common.php
@@ -39,3 +39,54 @@ function getPaginationDetails($method = "GET"){
             break;
     }
 }
+
+function getUniqueApplicationId($subsId,$format,$checkWebId = false){
+    
+        if (empty($format)) {
+            return false;
+        }
+        
+        if (empty($subsId)) {
+            throw new Exception("Subs Id missing", 404);
+        }
+        
+        try{
+        // find last application number created.
+            $whereQuery = " subs_id = ".$subsId;
+            if($checkWebId){
+                $whereQuery = " pay_id =".$subsId;
+            }
+        $lastPayId = db_query( "SELECT `pay_id` FROM `payment`  WHERE ".$whereQuery);
+        $lastPayId= intval($lastPayId[0]['pay_id']);
+        
+        $applicationNumber = $format. date('md'). $lastPayId;
+
+        
+        }catch(Exception $e){
+            throw new Exception("There was database error");
+        }
+        
+        return $applicationNumber;
+}
+
+function updateApplicationNumber($applicationNumber, $subsId){
+    
+    try{
+        // save application number in payment table
+        db_exec("UPDATE payment SET app_num = " . db_sanitize($applicationNumber) . " WHERE subs_id = " . $subsId . ";");
+        }catch(Exception $e){
+            throw new Exception("There was database error");
+        }
+    
+       
+}
+function getCourseApplicationFormat($courseId){
+    
+    $courseFormat = db_query("SELECT app_num_format FROM course WHERE course_id=" . db_sanitize($courseId) . ";");
+    
+    if(!empty($courseFormat)){
+        return $courseFormat[0]['app_num_format'];
+    }
+    
+    return false;
+}
