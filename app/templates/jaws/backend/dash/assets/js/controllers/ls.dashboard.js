953f5defe3a38a3c9ab187fd8ca05dd21455f276
ritesh-techjini <ritesh.kumar@digital.datamatics.com>
2020-03-26 10:10:02 +0530

    CR Feedback  Resolved

--- app/templates/jaws/backend/dash/assets/js/controllers/ls.dashboard.js
+++ app/templates/jaws/backend/dash/assets/js/controllers/ls.dashboard.js
@@ -13,13 +13,12 @@ angular.module('jaws')
       $scope.leadCompiledStatusArr = [{'id':'1','name':'New'},{'id':'2','name':'API requeste'},{'id':'3','name':'Success'},{'id':'4','name':'Failure'},{'id':'9','name':'Old Data'}];
       $scope.pageHeader = [{"id":0,"name":"Lead ID"},{"id":0,"name":"Lead Name"},{"id":0,"name":"Lead Phone"},{"id":0,"name":"Lead Date"}];
       $scope.leadStatusArr = $scope.leadBasicStatusArr;
-
-      $scope.loadLsDashboard = function(leadStatus, leadTable, leadList=''){
+      $scope.loadLsDashboard = function(){
          var param = {  
                         params:{
-                        "leadStatus": leadStatus,
-                        "leadTable": leadTable,
-                        "leadList":leadList
+                        "leadStatus": $scope.leadStatus,
+                        "leadTable": $scope.leadTable,
+                        "leadList":$scope.leadList
                       }
                     };  
            $http.get(_JAWS_PATH_API + 'lsDashboard', param).then(function (response) {
@@ -28,23 +27,17 @@ angular.module('jaws')
             });
        }
      $scope.leadSelectTable = function (){
-        if($scope.leadTable == 'basic'){
-            $scope.leadStatus='0';
-            $scope.leadStatusArr = $scope.leadBasicStatusArr;
-         }else{
-            $scope.leadStatus='1';
-            $scope.leadStatusArr = $scope.leadCompiledStatusArr;
-         }         
-         $scope.loadLsDashboard($scope.leadStatus,$scope.leadTable,$scope.leadList); 
+         $scope.leadStatus='0';
+         $scope.leadStatusArr = $scope.leadBasicStatusArr;
+       if($scope.leadTable == 'compiled'){
+         $scope.leadStatus='1';
+         $scope.leadStatusArr = $scope.leadCompiledStatusArr;
+        }           
+         $scope.loadLsDashboard(); 
      }
      $scope.selectLeadStatus = function (){
-
-        $scope.loadLsDashboard($scope.leadStatus,$scope.leadTable,$scope.leadList);
+        $scope.loadLsDashboard();
      }
-     $scope.selectLeadList = function(){
-
-        $scope.loadLsDashboard($scope.leadStatus,$scope.leadTable,$scope.leadList);
-    }   
-     $scope.loadLsDashboard($scope.leadStatus,$scope.leadTable,$scope.leadList);
+     $scope.loadLsDashboard();
     }]);
    /*  End JA-113 : LS Dashboard */
