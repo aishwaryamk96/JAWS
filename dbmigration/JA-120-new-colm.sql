fddbd51c5882f9c6b71399747b33e83404c7cc1f
Medini <medini.m@techjini.com>
2020-03-30 14:32:22 +0530

    JA-121 db col changes

--- dbmigration/JA-120-new-colm.sql
+++ dbmigration/JA-120-new-colm.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `course` ADD `payment_gateway` TINYINT NULL DEFAULT '0' COMMENT '1= New Razorpay account, 0 = Old gateway options' AFTER `platform_id`;
-ALTER TABLE `course_bundle` ADD `payment_gateway` TINYINT NULL DEFAULT '0' COMMENT '1= New Razorpay account, 0 = Old gateway options' AFTER `platform_id`;
-ALTER TABLE `course` CHANGE `payment_gateway` `rpay_acc_flag` TINYINT(4) NULL DEFAULT '0' COMMENT '1= New Razorpay account, 0 = Old gateway options';
+ALTER TABLE `course` ADD `rpay_acc_flag` TINYINT NULL DEFAULT '0' COMMENT '1= New Razorpay account, 0 = Old gateway options' AFTER `platform_id`;
+ALTER TABLE `course_bundle` ADD `rpay_acc_flag` TINYINT NULL DEFAULT '0' COMMENT '1= New Razorpay account, 0 = Old gateway options' AFTER `platform_id`;
