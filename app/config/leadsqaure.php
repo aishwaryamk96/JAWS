5fc91b3950f2f1e28651ed53609ae32dc9d04901
Medini <medini.m@techjini.com>
2020-05-08 14:35:41 +0530

    LS Account crednetials changes

--- app/config/leadsqaure.php
+++ app/config/leadsqaure.php
@@ -19,8 +19,11 @@ define("LS_SecretKey", "95c34e5756021c31bc0ded96a0fafd70320cd9a2");
         
 //define("LS_ACCOUNT_RETAIL_NEW_ACCESS",'u$ra77415d9fb3b4476342af0bea19a6b57');
 //define("LS_ACCOUNT_RETAIL_NEW_SECRET",'1b49151116bf144653207c80d7df51692847ceed');
-define("LS_ACCOUNT_RETAIL_NEW_ACCESS",'u$rc7986d7d4dfc4a76a4123b7d92a5f379');
-define("LS_ACCOUNT_RETAIL_NEW_SECRET",'cff6d61151836432d652775cea7c3e6088211ba1');
+//Disabling it on May 8, 2020 @2.30PM
+//define("LS_ACCOUNT_RETAIL_NEW_ACCESS",'u$rc7986d7d4dfc4a76a4123b7d92a5f379');
+//define("LS_ACCOUNT_RETAIL_NEW_SECRET",'cff6d61151836432d652775cea7c3e6088211ba1');
+define("LS_ACCOUNT_RETAIL_NEW_ACCESS",'u$rc9e44fac33d7cdbd37e0bc08633faa14');
+define("LS_ACCOUNT_RETAIL_NEW_SECRET",'c695c883ec8aa10bc9b11700d6865cd616077f71');
 
 //Basic Lead error log file name with path
 define("BASIC_LEAD_LOG",'/var/log/apache2/leadcron/basiclead.log');
