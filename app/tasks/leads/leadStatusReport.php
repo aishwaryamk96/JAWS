0a81fa416e41305d4a3c78b28356b25f6e574105
Aishwarya M K <aishwarya.mk@techjini.com>
2020-05-21 11:30:36 +0530

    JA-127 fix adding cron job and email template for failes lead list

---
+++ app/tasks/leads/leadStatusReport.php
@@ -0,0 +1,59 @@
+<?php
+
+// Prevent exclusive access
+if (!defined("JAWS")) {
+    header('Location: '.WEBSITE_URL);
+    die();
+}
+
+load_library("email");
+
+const  LEADBASICCOMPILEDFAILURESTATUS =4;
+const  LEADBASICFAILURESTATUS =2;
+
+$csvHeaders= "LEAD_ID LEAD_NAME LEAD_EMAIL LEAD_PHONE LEAD_DATE \r\n";
+$todaysDate = date('Y-m-d H:i:s');
+//from date can be vary according to the requirement
+$fromDate = date('Y-m-d H:i:s', strtotime('-3 day',strtotime($todaysDate)));
+
+//file handler
+$filename = "failed_leads".date('Y-m-d').".csv";
+$fileHandler = fopen($filename,"w+");
+fwrite($fileHandler, $csvHeaders);
+
+$count = processTheLeadToCsv($fileHandler,"user_leads_basic",LEADBASICFAILURESTATUS,$fromDate,$todaysDate);
+$count += processTheLeadToCsv($fileHandler,'user_leads_basic_compiled',LEADBASICCOMPILEDFAILURESTATUS,$fromDate,$todaysDate);
+fclose($fileHandler);
+$content["todate"] = date('d-M-Y H:i');
+$content["fromdate"] = date('d-M-Y H:i', strtotime('-3 day',strtotime($todaysDate)));
+$content["leadCount"] = $count;
+
+$emailInfo =array();
+//send an email
+if($count >0)
+    send_email_with_attachment("lead.fail.re",$emailInfo,$content,$filename);
+else
+    send_email("lead.fail.re",$emailInfo,$content);
+
+//delete the file
+unlink($filename);
+exit(0);
+
+function processTheLeadToCsv($fileHandler,$table,$status,$fromDate,$todaysDate){
+    $leadQuery = "SELECT  l.lead_id as id, l.name as name, l.email as email,l.phone as phone ,l.create_date as createDate";
+    $leadQuery .= " FROM ".$table." as l ";
+    $leadQuery .= " WHERE l.status = ".$status." AND  ";
+    $leadQuery .= " DATE(l.create_date) BETWEEN '".$fromDate."' AND '".$todaysDate."'";
+    $leadQuery .= " ORDER BY l.create_date DESC";
+    $leeds = db_query($leadQuery);
+    $count = db_count($leadQuery);
+    $line="";
+    foreach ($leeds as $leed) {
+        foreach ($leed as $key=>$val) {
+            $line .=preg_replace('/\s+/','',$val)." ";
+        }
+        $line .="\r\n";
+        fwrite($fileHandler,$line);
+    }
+    return $count;
+}
