6aafc740b154b9c6b8c39ed9e6c3d5d8b7399aa8
Kailas <kailasb@imorsetech.com>
2020-05-27 12:12:45 +0530

    TK064 - TK064-corporate-lead-email.sql -> added insert query in "system_email" table for corporate lead email.

---
+++ dbmigration/TK064-corporate-lead-email.sql
@@ -0,0 +1,1 @@
+INSERT INTO `system_email` (`template_id`, `template_path`, `desc`, `subject`, `sender`, `recipient`, `copied`, `copied_blind`) VALUES ('corporate.email.lead', 'corporate/corporate_lead', 'send email of corporate lead to specific email mentioned in email. ', 'Corporate Lead', 'info@jigsawacademy.com,Jigsaw Academy', NULL, NULL, 'jagruti@jigsawacademy.com');
